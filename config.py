69503041a5e3f02e0a0209c8b01583976340761e
Marc Daya <marc@sinistral.net>
2016-06-25 13:43:12 -0600

    Wire up file upload routes.
    Two new API routes have been added: one to upload a file in parts, and
    another to signal the completion of upload and reconstitute the file
    from its parts.
    
    This initial commit of this functionality does reconstitute the file,
    but doesn't yet allocate it to the appropriate sample stage.  It is
    anticipated that these uploaded files will be archives containing
    the many files produced by a scan or analysis stage, and these will need
    to be unpacked and put into an appropriate location.
    
    It is also because of this additional server-side processing that we
    respond to a successful file upload completion request with a 202,
    rather than the default 200.  The 202 response code indicates that all
    client interaction has been successfully handled, but that further
    server-side processing is required before the resource will become
    available.

--- config.py
+++ config.py
@@ -9,8 +9,10 @@ SECRET_KEY = ']`<{e&b$D5)tzd)>242KyFGz8jEZzk8:'
 
 THREADS_PER_PAGE = 8
 
-UPLOAD_PATH = '/mnt/adapt/.upload'
 STORE_PATH = '/mnt/adapt'
+UPLOAD_PATH = os.path.join(STORE_PATH, '.upload')
+
+MAX_CONTENT_LENGTH = (1 * 1024 * 1024) + (512 * 1024)
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')
