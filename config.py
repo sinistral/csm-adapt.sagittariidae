90a05d9632187cfc44d6692435346b9023815406
Marc Daya <marc@sinistral.net>
2016-09-13 10:40:27 -0600

    Deployment preparation.
    * Serve static site data.
    
    Configure Werkzeug to serve the site files: static HTML and
    {Clojure,Java}Script.  The 'static' directory from which there are
    served are not part of this package, but will be created during
    deployment.
    
    * Change database location.
    
    The database has been moved to a location that is more consistent with
    the Linux FS standard.  Separate from the project source, persistance
    can be achieved by using the directory as a mount point for a Docker
    volume.

--- config.py
+++ config.py
@@ -9,11 +9,20 @@ SECRET_KEY = ']`<{e&b$D5)tzd)>242KyFGz8jEZzk8:'
 
 THREADS_PER_PAGE = 8
 
-STORE_PATH = '/mnt/adapt'
+# The location from which we'll serve the static site pages and *Script source
+# files.
+STATIC_ROOT = 'static'
+# The permanent home for uploaded data files, and the directory from which they
+# will be served.
+STORE_PATH  = '/mnt/adapt'
+# Location in which uploaded files are staged until verified and moved into
+# their permanent home.
 UPLOAD_PATH = os.path.join(STORE_PATH, '.upload')
 
+# The maximum size of a request message.  This is constrained to prevent us
+# from being swamped by clients trying to upload large files in one request.
 MAX_CONTENT_LENGTH = (1 * 1024 * 1024) + (512 * 1024)
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db/db_repository')
+SQLALCHEMY_DATABASE_URI = 'sqlite:////var/db/sagittariidae/sagittariidae.db'
+SQLALCHEMY_MIGRATE_REPO = '/var/db/sagittariidae/db_repository'
